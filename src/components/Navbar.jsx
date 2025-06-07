2f441d37728592d61467e3bf05b156eb70a8e493
Your Name <user@example.com>
2025-06-07 13:31:50 +0800

    Add Navbar component and update App structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div className=''>
+        {/* LOGO */}
+        <div className="">
+            <img src="/logo.png" className="w-8 h-8" alt="" />
+            <span>lamaLog</span>
+        </div>
+        {/* MOBILE MENU */}
+        <div className=""> M </div>
+        {/* DESKTOP MENU */}
+        <div className=""> D </div>
+    </div>
+  )
+}
+
+export default Navbar
